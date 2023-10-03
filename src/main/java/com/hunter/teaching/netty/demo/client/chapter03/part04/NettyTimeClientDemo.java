9364388c9c60827d0230e4d10b2f55f24df6df2f
hunter <babysai1988@vip.qq.com>
2023-10-03 14:35:37 +0800

    LengthFieldBasedFrameDecoder & LengthFieldPrepender

---
+++ src/main/java/com/hunter/teaching/netty/demo/client/chapter03/part04/NettyTimeClientDemo.java
@@ -0,0 +1,61 @@
+package com.hunter.teaching.netty.demo.client.chapter03.part04;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldPrepender;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * @author hunter
+ * @since May 7th
+ * @note This Demo considers the tcp stick package or tcp unpacking issues.
+ *       We send 1000 messages to the server and no issue will appear.
+ */
+public class NettyTimeClientDemo {
+
+    public void connect(String ip, int port) throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+            .channel(NioSocketChannel.class)
+            .option(ChannelOption.TCP_NODELAY, true)
+            .handler(new NettyHandlerClientDemo());
+            // async connect operation
+            ChannelFuture f = b.connect(ip, port).sync();
+            // wait the client close the channel
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    private class NettyHandlerClientDemo extends ChannelInitializer<SocketChannel> {
+
+        @Override
+        protected void initChannel(SocketChannel sc) throws Exception {
+            // LengthFieldBasedFrameDecoder slove the tcp stick package or tcp unpacking issues
+            sc.pipeline().addLast(new LengthFieldBasedFrameDecoder(65535, 0, 2, 0, 2));
+            sc.pipeline().addLast(new LengthFieldPrepender(2));
+            // StringDecoder transfer the bytebuf object to string object automatically
+            sc.pipeline().addLast(new StringDecoder());
+            sc.pipeline().addLast(new NettyTimeClientHandlerDemo());
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        NettyTimeClientDemo nettyTimeClientDemo = new NettyTimeClientDemo();
+        nettyTimeClientDemo.connect("127.0.0.1", 10080);
+    }
+
+}
---
+++ src/main/java/com/hunter/teaching/netty/demo/client/chapter03/part04/NettyTimeClientHandlerDemo.java
@@ -0,0 +1,44 @@
+package com.hunter.teaching.netty.demo.client.chapter03.part04;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class NettyTimeClientHandlerDemo extends ChannelInboundHandlerAdapter {
+
+    private static final String CLIENT_COMMAND = "Query Time Order";
+
+    private final AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    /**
+     * send 1000 messages to the server
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        byte[] byteArr = CLIENT_COMMAND.getBytes();
+        for (int i = 0; i < 1000; i++) {
+            ByteBuf byteBuf = Unpooled.copiedBuffer(byteArr);
+            ctx.writeAndFlush(byteBuf);
+        }
+    }
+
+    /**
+     * It will not appear the tcp stick package and tcp unpacking issues.
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String message = (String) msg;
+        int count = atomicInteger.incrementAndGet();
+        System.out.println("Now is : " + message + ", count = " + count);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.err.print("Unexpected exception from downstream : " + cause.getMessage());
+        ctx.close();
+    }
+
+}
